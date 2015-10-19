671bf1fe5ca2665606c411d81650ab5896b7df0c
Steven Eksteen <steven@secondimpression.net>
2015-10-19 07:34:41 +0200

    'In-house' assets

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,29 +1,4 @@
-ellipsis = {
-  'value' : ['&nbsp;', '.', '..', '...'],
-  'count' : 0,
-  'run' : false,
-  'timer' : null,
-  'element' : '.loading',
-  'start' : function () {
-    var t = this;
-    this.run = true;
-    this.timer = setInterval(function () {
-      if (t.run) {
-        $(t.element).html(t.value[t.count % t.value.length]).text();
-        t.count++;
-      }
-    }, 250);
-  },
-  'stop' : function () {
-    this.run = false;
-    clearInterval(this.timer);
-    this.count = 0;
-  }
-}
-
-ellipsis.start();
-
-$( document ).ready(function() {
+$(document).ready(function() {
   $('.indentation-container' ).on('click', '.indentation.indentation-toggle', function() {
     $( this ).closest( '.indentation-container' ).addClass( 'collapsed' );
   });
@@ -31,4 +6,6 @@ $( document ).ready(function() {
   $('.indentation-container' ).on('click', '.indentation-ellipsis.indentation-toggle', function() {
     $( this ).closest( '.indentation-container' ).removeClass( 'collapsed' );
   });
+
+  $('[data-toggle="tooltip"]').tooltip();
 });
